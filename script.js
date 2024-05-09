5abb03ab2448a436d2e3db257c26aa8e28ae940c
SAKSHI12gangwar <sakkugangwar11@gmail.com>
2024-05-09 04:43:23 -0700

    calcultor

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ arr.forEach(button => {
         if(e.target.innerHTML == '='){
             string = eval(string);
             input.value = string;
+            
         }
 
         else if(e.target.innerHTML == 'AC'){
